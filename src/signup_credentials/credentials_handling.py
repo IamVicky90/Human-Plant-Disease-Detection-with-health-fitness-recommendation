4d0033f31cfc30b21ec10259ae0ef08cbe21de8e
IamVicky90 <waqasbilal02@gmail.com>
2021-11-21 22:28:49 +0500

    logs sucessfully added to credentials_handling.py

--- src/signup_credentials/credentials_handling.py
+++ src/signup_credentials/credentials_handling.py
@@ -1,17 +1,30 @@
 from src.mongo_db_ops.db_operations import mongo_db_atlas_ops
+from src.loggings import add_logger
 class sign_up_credentials:
     def __init__(self):
         self.mongo_conn=mongo_db_atlas_ops()
     def credentials_validations(self,email,password):
         '''See whether email and password is already regestered or not'''
-        client=self.mongo_conn.get_mongo_db_connection()
-        login_credentials=client['Human_and_Plant_Health_SDM']
-        email_validation_flag, password_validation_flag=self.mongo_conn.find_in_database(login_credentials,str(email),str(password))
-        return email_validation_flag, password_validation_flag
+        try:
+            log=add_logger()
+            client=self.mongo_conn.get_mongo_db_connection()
+            login_credentials=client['Human_and_Plant_Health_SDM']
+            email_validation_flag, password_validation_flag=self.mongo_conn.find_in_database(login_credentials,str(email),str(password))
+            log.log(f'credentials_validations funtion run sucessfully and the email_validation_flag: {email_validation_flag} and password_validation_flag: {password_validation_flag} for email {str(email)}','credentials_handling.log',1)
+            return email_validation_flag, password_validation_flag
+        except Exception as e:
+            log.log(f'credentials_validations could not run sucessfully for email {str(email)}, error, {str(e)}','credentials_handling.log',3)
+            
     def dump_credentials_to_mongo_atlas(self,fname,lname,email,password,city,state,zip):
-        email_validation_flag, _=self.credentials_validations(email,password)
-        if not email_validation_flag:
-            return self.mongo_conn.insert_signup_credentials(fname,lname,email,password,city,state,zip)
-        return "<h1>Your Email is already registered with us if you forget your password then please click to the foget password.</h1>"
+        log=add_logger()
+        try:
+            email_validation_flag, _=self.credentials_validations(email,password)
+            if not email_validation_flag:
+                log.log(f'dump_credentials_to_mongo_atlas funtion run sucessfully and email {str(email)} is sucessfully registered with us at signup form','credentials_handling.log',1)
+                return self.mongo_conn.insert_signup_credentials(fname,lname,email,password,city,state,zip)
+            log.log(f'dump_credentials_to_mongo_atlas funtion run sucessfully and email {str(email)} is already registered with us at signup form','credentials_handling.log',2)
+            return "<h1>OOPS! Your Email is already registered with us if you forget your password then please click to the foget password at the login page!</h1>"
+        except Exception as e:
+            log.log(f'dump_credentials_to_mongo_atlas could not run sucessfully for email {str(email)}, error, {str(e)}','credentials_handling.log',3)
         
         
